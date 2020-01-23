7e0b8edcba524f55d6111681141ced6f6c60adae
Madson Cinq <madson.santana96@gmail.com>
2020-01-23 11:59:08 -0300

    > teste de requisicao pela API

--- Main.ts
+++ Main.ts
@@ -1,6 +1,25 @@
 import Movie from "./Classes/Movie"
 import TvSeries from "./Classes/TvSeries"
 
-let filme1 = new Movie("movie - the movie")
-
-console.log(filme1)
+const apiKey = "0e3ee1b7858bef88517145e3ed37ca91";
+let movieId = 1412;
+let apiUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
+let httpRequestAsync = (method, url) => {
+    return new Promise<string>(function(resolve, reject) {
+      var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          resolve(xhr.responseText);
+        } else {
+          reject(new Error(xhr.responseText));
+        }
+      };
+      xhr.send();
+    });
+  };
+  
+  httpRequestAsync("get", apiUrl)
+    //.then(res => console.log(res))
+    .catch(error => console.log(error));
