1513180c0bc920fca3d33c62af31699b90701df9
Madson Cinq <madson.santana96@gmail.com>
2020-01-23 09:42:31 -0300

    > separacao das classes em seus respectivos arquivos

---
+++ Classes/Season.ts
@@ -0,0 +1,11 @@
+export default class Season{
+    air_date: string
+    episode_count: number
+    id: number
+    name: string
+    overview: string
+    poster_path: string
+    season_number: number
+
+    constructor(){}
+}
---
+++ Classes/Spoken_lang.ts
@@ -0,0 +1,6 @@
+export default class Spoken_lang{
+    iso_639_1: string
+    name: string
+
+    constructor(){}
+}
